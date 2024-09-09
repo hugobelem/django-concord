65132e22feccf6d734aa1fd31418201c1910f3b5
Hugo Belém <belem.hugo@hotmail.com>
2024-09-09 20:47:23 -0300

    finished app

--- backend/base/forms.py
+++ backend/base/forms.py
@@ -1,6 +1,17 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Room, User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name',
+                  'username',
+                  'email',
+                  'password1',
+                  'password2']
 
 
 class RoomForm(ModelForm):
@@ -13,8 +24,11 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['avatar' , 'bio', 'name', 'username', 'email']
         labels = {
+            'avatar': 'Avatar',
+            'bio': 'Bio',
+            'name': 'Name',
             'username': 'Username',
             'email': 'Email',
         }
--- backend/studybud/urls.py
+++ backend/studybud/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from base import views
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', include('base.urls'), name='base'),
     path("api/", include('base.api.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
